10c9dffac9b883b394d2e7d7b6319f290bb5e727
Sladzio <sladzio995@gmail.com>
2019-03-07 15:52:20 +0100

    *Added scraping date,lottery number, winning numbers

--- scraper/scraper/spiders/lotto_spider.py
+++ scraper/scraper/spiders/lotto_spider.py
@@ -12,10 +12,20 @@ class LottoSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        years_url_list = response.xpath(
-            "//div[@class='list_of_last_drawings lotto_list_of_last_drawings']/p[@class='lista_lat']/a/@href").extract()
+        years_url_list = response.css("p.lista_lat a::attr(href)").extract()
         for url in years_url_list:
-            print url
-# response.xpath("//ul/li[@class='nr_in_list']/text()").extract()
-#response.xpath("//ul/li[@class='date_in_list']/text()").extract()
-#response.xpath("//ul/li[@class='numbers_in_list ']/text()").extract()
+            yield scrapy.Request(url=url, callback=self.parse_years_page)
+
+    def parse_years_page(self, response):
+        yield {
+            'lottery_nr': response.css("ul li.nr_in_list::text").re("\d+"),
+            'date': response.css("ul li.date_in_list::text").extract(),
+            'winning': self.parse_winning_numbers(response.css("ul li.numbers_in_list::text").re("\d+"))
+        }
+
+    def parse_winning_numbers(self, all_numbers):
+        result = []
+        for index in xrange(0, len(all_numbers) - 6, 6):
+            single_draw_result = all_numbers[index:index + 6]
+            result.append(",".join(single_draw_result))
+        return result
